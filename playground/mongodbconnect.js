731956dbfb4d10daadbc9c43b5980e0f0ef61245
Imtiyaz Mohammed <imtiyazmohammed@Imtiyazs-MacBook-Pro.local>
2018-07-11 22:59:34 -0700

    Added the code for MongoDB insert and find and delete

--- playground/mongodbconnect.js
+++ playground/mongodbconnect.js
@@ -10,76 +10,56 @@
 
 
 // const MongoClient = require('mongodb').MongoClient;
-
-// const {MongoClient, ObjectId} = require('mongodb');
-// // var obj = new ObjectId();
-// // console.log(obj);
-//
-// const URL = 'mongodb://localhost:27017/Users';
-// MongoClient.connect(URL,(err, client)=>{
-//     if(err){
-//       return console.log('unable to connect to database', err);
-//     }
-//     const db = client.db('TodoApp');
-//     console.log('succesfully connected to the database');
-//
-//     db.collection('TodoApp').insertOne({
-//       text:'submit timesheet in paycor',
-//       completed: false
-//     }, (err, result)=>{
-//       if(err){
-//         return console.log('unable to insert data into the TodoApp', err);
-//       }
-//       console.log('Inserted data into the database', result.ops);
-//     })
-//     db.close();
-// });
-//
-// MongoClient.connect(URL, (err, client)=>{
-//   if(err){
-//     return console.log('unable to connect to MongoClient', err);
-//   }
-//   var db = client.db('Users');
-//   console.log('succesfully connected to MongoClient');
-//
-//   db.collection('Users').insertOne({
-//     name: 'Imtiyaz',
-//     age: 27,
-//     location:'Seattle'
-//   }, (err, result)=>{
-//     if(err){
-//       return console.log('Unable to insert document into Users collection');
-//     }
-//     console.log(result.ops);
-//   })
-//   db .close();
-// });
-
 const {MongoClient, ObjectId} = require('mongodb');
-URL = 'mongodb://localhost:27017/Users'
-MongoClient.connect(URL, (err, client)=>{
-  if(err){
-    return console.log('error connecting to mongodb', err);
-  }
-  var db = client.db('Users');
+// // var obj = new ObjectId();
+// console.log(obj);
 
-  // db.collection('Users').find({}).toArray().then((docs)=>{
-  //   console.log(docs)
-  //   client.close();
-  //   }
-  //   ).catch((err) => {
-  //     if(err)
-  //     console.log(err);
-  //   });
+const URL = 'mongodb://localhost:27017/Users';
+MongoClient.connect(URL,(err, client)=>{
+    if(err){
+      return console.log('unable to connect to database', err);
+    }
+    const db = client.db('TodoApp');
+    console.log('succesfully connected to the database');
 
-  // db.collection('Users').find({}).count().then((count)=>{
-  //   console.log(`The count of Todos is: ${count}`)
-  //   client.close();
-  //   }
-  //   ).catch((err) => {
-  //     if(err)
-  //     console.log(err);
-  //   });
+    db.collection('TodoApp').insertOne({
+      text:'submit timesheet in paycor',
+      completed: false
+    }, (err, result)=>{
+      if(err){
+        return console.log('unable to insert data into the TodoApp', err);
+      }
+      console.log('Inserted data into the database', result.ops);
+    })
+
+  db.collection('Users').insertOne({
+    name: 'Imtiyaz',
+    age: 27,
+    location:'Seattle'
+  }, (err, result)=>{
+    if(err){
+      return console.log('Unable to insert document into Users collection');
+    }
+    console.log(result.ops);
+  })
+
+  db.collection('Users').find({}).toArray().then((docs)=>{
+    console.log(docs)
+    client.close();
+    }
+    ).catch((err) => {
+      if(err)
+      console.log(err);
+    });
+
+  db.collection('Users').find({}).count().then((count)=>{
+    console.log(`The count of Todos is: ${count}`)
+    client.close();
+    }
+    ).catch((err) => {
+      if(err)
+      console.log(err);
+    });
 
   db.collection('Users').find({name:'Mohammed'}).count().then((count)=>{
     console.log(`The count of Todos is: ${count}`)
@@ -98,4 +78,33 @@ MongoClient.connect(URL, (err, client)=>{
       if(err)
       console.log(err);
     });
+
+  db.collection('Users').find({name:'Mohammed'}).toArray().then((docs)=>{
+    console.log(docs);
+  }, (err)=>{
+    if(err){
+      console.log('error finding the documents', err);
+    }
+    db.close();
+  });
+
+  db.collection('Users').deleteOne({name:'Imtiyaz'});
+
+  db.collection('Users').aggregate([{$group:{_id: '$name',total: {$sum: 1}}}]).
+  toArray().then((docs)=>{
+    docs.forEach((iterator, callback)=>{
+      if(iterator.total > 1){
+      db.collection('Users').deleteMany({name:iterator._id});
+    }
+  })}, (err)=>{
+    if(err){
+      console.log(err);
+    }
+  });
+
+  db.collection('Users').findOneAndDelete({name:'Rajiv'}).then((docs)=>{
+    console.log(docs);
+  });
+
+  // client.close();
 });
