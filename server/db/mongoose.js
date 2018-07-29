c2c00179bd2f366e12aae0c783d13b7d9531687c
Imtiyaz Mohammed <imtiyazmohammed@Imtiyazs-MacBook-Pro.local>
2018-07-29 08:56:44 -0700

    updated mongo url parser

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,9 +3,10 @@ mongoose.Promises = global.Promises
 // var MONGODB_URI_HEROKU = process.env.MONGODB_URI;
 // mongoose.connect(MONGODB_URI_HEROKU || 'mongodb://localhost:27017/TodoApp');
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err)=>{
-    if(!err){
-    console.log(`error connecting to mongodb with URL: ${process.env.MONGODB_URI} and error is: ${err}`);
+    if(!err && err !== null){
+        return console.log(`error connecting to mongodb with URL: ${process.env.MONGODB_URI} and error is: ${err}`);
     }
+    console.log(`succesfully connected to mongodb with URL: ${process.env.MONGODB_URI}`);
 });
 
 
