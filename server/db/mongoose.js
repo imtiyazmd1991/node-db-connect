bab509fffff7e83346f2ff0805cdbc0bf41f309e
Imtiyaz Mohammed <imtiyazmohammed@Imtiyazs-MacBook-Pro.local>
2018-07-28 23:58:51 -0700

    comm

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.Promises = global.Promises
-var MONGODB_URI_HEROKU = process.env.MONGODB_URI;
-mongoose.connect(MONGODB_URI_HEROKU || 'mongodb://localhost:27017/TodoApp');
+// var MONGODB_URI_HEROKU = process.env.MONGODB_URI;
+// mongoose.connect(MONGODB_URI_HEROKU || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 
 module.exports = {mongoose};
